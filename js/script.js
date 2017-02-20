f72f972e6a8f9a1ce98d1ef812eba922d5fe922b
SergeyZakharov1979 <sergun.zahar79@gmail.com>
2017-02-21 00:38:25 +0600

    Завершил вёрстку проекта

--- js/script.js
+++ js/script.js
@@ -1,46 +1,104 @@
+<!--Интерактивная карта-->
 var viewmap = document.querySelector(".geolocation-company .map");
 var popmap = document.querySelector(".modal-content-map");
 var closemap = document.querySelector(".modal-content-close");
 
-viewmap.addEventListener("click", function(event) {
-	event.preventDefault();
-	popmap.classList.add("modal-content-map-show");
-});
+if(viewmap) {
+  viewmap.addEventListener("click", function(event) {
+    event.preventDefault();
+    popmap.classList.add("modal-content-map-show");
+  });
 
-closemap.addEventListener("click", function(event) {
-	event.preventDefault();
-	popmap.classList.remove("modal-content-map-show");
-});
+  closemap.addEventListener("click", function(event) {
+    event.preventDefault();
+    popmap.classList.remove("modal-content-map-show");
+  });
 
+  window.addEventListener("keydown", function(event) {
+  	if (event.keyCode === 27) {
+  		if (popmap.classList.contains("modal-content-map-show")) {
+  				popmap.classList.remove("modal-content-map-show");
+  		}
+  	}
+  });
+}
 
+<!--Всплывающее окно отправки формы-->
 var viewwrite = document.querySelector(".geolocation-company .btn");
 var popwrite = document.querySelector(".write-us");
 var closewrite = document.querySelector(".write-us-btn-close");
-var writename = popwrite.querySelector("[name=name]");
-var form = popwrite.querySelector("form");
 
-viewwrite.addEventListener("click", function(even) {
-	even.preventDefault();
-	popwrite.classList.add("write-us-show");
-	writename.focus();
-	console.log("JS working");
-});
+if(viewwrite) {
+  var writename = popwrite.querySelector("[name=name]");
+  var form = popwrite.querySelector("form");
+  var email = popwrite.querySelector("[name=email]");
+  var storage = localStorage.getItem("name");
+  var stor = localStorage.getItem("mail");
+  var text = popwrite.querySelector("textarea");
 
-closewrite.addEventListener("click", function(even) {
-	even.preventDefault();
-	popwrite.classList.remove("write-us-show");
-});
+  viewwrite.addEventListener("click", function(event) {
+    event.preventDefault();
+    popwrite.classList.add("write-us-show");
+    
+    if(storage && stor) {
+    	writename.value = storage;
+    	email.value = stor;
+    	text.focus();
+    }	else {
+    		writename.focus();
+    }
+  });
 
-var viewcart = document.querySelector(".product-action .buy");
-var popcart = document.querySelector(".items-in-cart");
+  closewrite.addEventListener("click", function(event) {
+    event.preventDefault();
+    popwrite.classList.remove("write-us-show");
+    popwrite.classList.remove("write-us-error");
+  });
+
+  form.addEventListener("submit", function(event) {
+    if (!writename.value || !email.value) {
+    event.preventDefault();
+    popwrite.classList.add("write-us-error");
+  	} else { 
+  		localStorage.setItem("name", writename.value);
+  		localStorage.setItem("mail", email.value);
+  	}
+  });
+
+  window.addEventListener("keydown", function(event) {
+  	if (event.keyCode === 27) {
+  		if (popwrite.classList.contains("write-us-show")) {
+  			popwrite.classList.remove("write-us-show");
+  			popwrite.classList.remove("write-us-error");
+  		}
+  	}
+  });
+}
+
+<!--Модальное окно: Товар добавлен в корзину-->
+var viewcart = document.querySelectorAll(".product-action .buy");
 var closecart = document.querySelector(".items-in-cart-btn-close");
 
-viewcart.addEventListener("click", function(eve) {
-	eve.preventDefault();
-	popcart.classList.add("items-in-cart-show");
-});
+if(viewcart && closecart) {
+  var popcart = document.querySelector(".items-in-cart");
 
-closecart.addEventListener("click", function(eve) {
-	eve.preventDefault();
-	popcart.classList.remove("items-in-cart-show");
-});
+  viewcart.forEach(function(item, i) {
+    item.addEventListener("click", function(event) {
+      event.preventDefault();
+      popcart.classList.add("items-in-cart-show");
+    });
+
+    closecart.addEventListener("click", function(event) {
+      event.preventDefault();
+      popcart.classList.remove("items-in-cart-show");
+    });
+  });
+
+  window.addEventListener("keydown", function(event) {
+  	if (event.keyCode === 27) {
+  		if (popcart.classList.contains("items-in-cart-show")) {
+  			popcart.classList.remove("items-in-cart-show");
+  		}
+  	}
+  });
+}
